abf15c13cb262152713940cd78d3fcab4bf75039
Japanjot19 <68852849+Japanjot19@users.noreply.github.com>
2023-01-08 00:44:01 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,14 @@
-echo "Kill all the running PM2 actions"
-pm2 kill
+echo "Install latest node"
+sudo npm cache clean -f
+sudo npm install -g n
+sudo n stable
 
 echo "Jump to app folder"
 cd /home/ubuntu/Elixir-Welcome-Bot
+
+echo "stop index.js"
+pm2 stop index.js
+pm2 delete index.js
 
 echo "Update app from Git"
 git pull
