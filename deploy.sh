08b638766c1983c8980d8f1d6a8c7d6aa79d0264
Japanjot <sjapanjot8@gmail.com>
2023-01-07 20:39:12 +0530

    Changed directory structure

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+echo "Kill all the running PM2 actions"
+sudo pm2 kill
+
+echo "Jump to app folder"
+cd /home/ubuntu/Elixir-Welcome-Bot
+
+echo "Update app from Git"
+git pull
+
+echo "Install app dependencies"
+sudo npm install
+
+echo "Run new PM2 action"
+sudo pm2 start index.js
